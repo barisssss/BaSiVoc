2190a6e0e003e18baed215613a9e239804b5e5f1
barisssss <barisaltun96@gmail.com>
2017-07-16 15:23:00 +0200

    simons shit

--- app/src/main/java/com/bamon/basivoc/OptionsActivity.java
+++ app/src/main/java/com/bamon/basivoc/OptionsActivity.java
@@ -1,8 +1,14 @@
 package com.bamon.basivoc;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.bamon.basivoc.db.DatabaseHelper;
@@ -13,18 +19,39 @@ import java.util.List;
 public class OptionsActivity extends AppCompatActivity {
 
     Spinner lang1, lang2;
+    EditText lengthET;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
+        prefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+
         lang1 = (Spinner) findViewById(R.id.spinner_language1);
         lang2 = (Spinner) findViewById(R.id.spinner_language2);
+        lengthET = (EditText) findViewById(R.id.practiceLengthInput);
+
+        lang1.setSelection(prefs.getInt());
 
         loadSpinnerData();
 
 
+    }
+
+    public void acceptOptions(View v){
+        int l1 = lang1.getSelectedItemPosition() + 1;
+        int l2 = lang2.getSelectedItemPosition() + 1;
+        int length = Integer.parseInt(lengthET.getText() + "");
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("currentLanguage1", l1);
+        editor.putInt("currentLanguage2", l2);
+        editor.putInt("currentPracticeLength", length);
+        editor.apply();
+
+        Intent i = new Intent(this, MenuActivity.class);
+        startActivity(i);
     }
 
     private void loadSpinnerData() {
--- app/src/main/java/com/bamon/basivoc/PracticeActivity.java
+++ app/src/main/java/com/bamon/basivoc/PracticeActivity.java
@@ -1,4 +1,6 @@
 package com.bamon.basivoc;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,17 +15,20 @@ public class PracticeActivity extends AppCompatActivity {
     Button knew;
     Button wrong;
     TextView va;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+        prefs = getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
+
         va = (TextView) findViewById(R.id.vocAnzeige);
         tA = (Button) findViewById(R.id.turnAroundButton);
         knew = (Button) findViewById(R.id.knewButton);
         wrong = (Button) findViewById(R.id.wrongButton);
         db = new DatabaseHelper(this, null, null, 1);
-        va.setText(db.getVocabItem(1, 1, 2).getPhrase1());
+        va.setText(db.getVocabItem(1, prefs.getInt("currentLanguage1", 1), prefs.getInt("currentLanguage2", 2)).getPhrase1());
 
     }
 
@@ -31,14 +36,14 @@ public class PracticeActivity extends AppCompatActivity {
         tA.setVisibility(View.GONE);
         knew.setVisibility(View.VISIBLE);
         wrong.setVisibility(View.VISIBLE);
-        va.setText(db.getVocabItem(1, 1, 2).getPhrase2());
+        va.setText(db.getVocabItem(1,prefs.getInt("currentLanguage1", 1), prefs.getInt("currentLanguage2", 2)).getPhrase2());
     }
 
     public void knewwrongClicked(View v){
         tA.setVisibility(View.VISIBLE);
         knew.setVisibility(View.GONE);
         wrong.setVisibility(View.GONE);
-        va.setText(db.getVocabItem(2, 1, 2).getPhrase1());
+        va.setText(db.getVocabItem(2, prefs.getInt("currentLanguage1", 1), prefs.getInt("currentLanguage2", 2)).getPhrase1());
     }
 
 
